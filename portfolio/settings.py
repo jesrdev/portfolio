ccc44775e83d7af51cf2521e7861667c8658e1ae
sanchezrivjesus <sanchezriv.jesus@gmail.com>
2021-07-04 21:40:04 -0500

    Settings changes

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-v#87m@p_3=9)b^=f8xcflp79%f+8%seq_c^$w$+xd0j&!!nh3#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
