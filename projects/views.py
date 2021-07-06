21a893250d6cc6b5000cc4452bb12e1938c33b74
sanchezrivjesus <sanchezriv.jesus@gmail.com>
2021-07-06 18:01:14 -0500

    Fixed content variable

--- projects/views.py
+++ projects/views.py
@@ -3,17 +3,11 @@ from django.shortcuts import render, get_object_or_404
 from .models import Project
 from django.forms import ModelForm
 
-class PhotoForm(ModelForm):
-    class Meta:
-        model = Project
-
 # Create your views here.
 def homepage(request):
-    context = dict(backend_form = PhotoForm())
     projects = Project.objects
-    return render(request, 'projects/home.html', context, {'projects':projects}) 
+    return render(request, 'projects/home.html', {'projects':projects}, None,) 
 
 def detail(request, project_id):
-    context = dict(backend_form = PhotoForm())
     project_detail = get_object_or_404(Project, pk=project_id)
-    return render(request, 'projects/detail.html', context, {'project':project_detail})
+    return render(request, 'projects/detail.html', {'project':project_detail}, None)
