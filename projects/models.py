5833c13275cfdd7987c38c4c8807191701d0a128
sanchezrivjesus <sanchezriv.jesus@gmail.com>
2021-07-05 20:12:54 -0500

    database change

--- projects/models.py
+++ projects/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Project(models.Model):
-    image = models.ImageField(upload_to='static/')
+    image = models.ImageField(upload_to='media/')
     summary = models.CharField(max_length=200)
     specifics = models.CharField(max_length=200)
     app_url = models.CharField(max_length=100)
