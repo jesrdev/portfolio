d17633e486b6f712b62cb08436803f6003c439a7
sanchezrivjesus <sanchezriv.jesus@gmail.com>
2021-07-05 20:09:08 -0500

    database change

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -127,8 +127,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR
+MEDIA_URL = '/static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
