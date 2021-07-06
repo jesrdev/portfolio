2a0ded7f1c2aaa83277f63a22f7c9f1c26438da4
sanchezrivjesus <sanchezriv.jesus@gmail.com>
2021-07-06 17:36:54 -0500

    Added context variable

--- projects/views.py
+++ projects/views.py
@@ -9,11 +9,11 @@ class PhotoForm(ModelForm):
 
 # Create your views here.
 def homepage(request):
-    context = NullBooleanField
+    context = dict(backend_form = PhotoForm())
     projects = Project.objects
     return render(request, 'projects/home.html', context, {'projects':projects}) 
 
 def detail(request, project_id):
-    context = NullBooleanField
+    context = dict(backend_form = PhotoForm())
     project_detail = get_object_or_404(Project, pk=project_id)
     return render(request, 'projects/detail.html', context, {'project':project_detail})
