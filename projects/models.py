e1830854c421399ae83b2bd64ac1af273abf009a
sanchezrivjesus <sanchezriv.jesus@gmail.com>
2021-07-05 19:01:52 -0500

    Added field to db

--- projects/models.py
+++ projects/models.py
@@ -6,6 +6,7 @@ class Project(models.Model):
     summary = models.CharField(max_length=200)
     specifics = models.CharField(max_length=200)
     app_url = models.CharField(max_length=100)
+    image_name = models.CharField(max_length=50)
 
     def __str__(self):
         return self.summary
