69d120ce24ff4bf3c840de66e3e86017f7a203a6
sanchezrivjesus <sanchezriv.jesus@gmail.com>
2021-07-13 16:56:21 -0500

    Work on context

--- projects/views.py
+++ projects/views.py
@@ -6,8 +6,8 @@ from django.forms import ModelForm
 # Create your views here.
 def homepage(request):
     projects = Project.objects
-    return render(request, 'projects/home.html', {'projects':projects}) 
+    return render(request, 'projects/home.html', {'projects':projects}, None, None, None) 
 
 def detail(request, project_id):
     project_detail = get_object_or_404(Project, pk=project_id)
-    return render(request, 'projects/detail.html', {'project':project_detail})
+    return render(request, 'projects/detail.html', {'project':project_detail}, None, None, None)
