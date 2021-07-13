c2c72660ca1f28b3a8fa5cabc124ba2a45c6340e
sanchezrivjesus <sanchezriv.jesus@gmail.com>
2021-07-13 16:36:15 -0500

    Created superuser

--- projects/views.py
+++ projects/views.py
@@ -6,7 +6,7 @@ from django.forms import ModelForm
 # Create your views here.
 def homepage(request):
     projects = Project.objects
-    return render(request, 'projects/home.html', {'projects':projects}, None,) 
+    return render(request, 'projects/home.html', {'projects':projects}, None) 
 
 def detail(request, project_id):
     project_detail = get_object_or_404(Project, pk=project_id)
