9b5d4645093a0c02725fd25a0ca3960c7af138a3
sanchezrivjesus <sanchezriv.jesus@gmail.com>
2021-07-13 18:23:44 -0500

    changed render method

--- projects/views.py
+++ projects/views.py
@@ -1,13 +1,17 @@
-from django.forms.fields import NullBooleanField
+from django import template
 from django.shortcuts import render, get_object_or_404
 from .models import Project
-from django.forms import ModelForm
+from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
 def homepage(request):
     projects = Project.objects
-    return render(request, 'projects/home.html', {'projects':projects}, None, None, None) 
+    template = loader.get_template('projects/home.html')
+    return HttpResponse(template.render({'projects':projects}, request))
+
 
 def detail(request, project_id):
     project_detail = get_object_or_404(Project, pk=project_id)
-    return render(request, 'projects/detail.html', {'project':project_detail}, None, None, None)
+    template = loader.get_template('projects/detail.html')
+    return HttpResponse(template.render({'project':project_detail}, request))
