91d956c0d7438425dd3bed092c7233df114607ac
sanchezrivjesus <sanchezriv.jesus@gmail.com>
2021-07-14 13:50:05 -0500

    Added TextField

--- projects/models.py
+++ projects/models.py
@@ -5,7 +5,7 @@ from cloudinary.models import CloudinaryField
 class Project(models.Model):
     image = CloudinaryField('image', null=True)
     summary = models.CharField(max_length=200)
-    specifics = models.CharField(max_length=200)
+    specifics = models.TextField(blank = True)
     app_url = models.CharField(max_length=100)
 
     def __str__(self):
